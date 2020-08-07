08171efba82e129b439f83ea75e0d2db43c2d1b6
Dibora D <diborad@yahoo.com>
2020-08-07 17:11:05 -0500

    added Jumbotron styling and {children} props - maybe need more styling

--- personal-library/src/App.js
+++ personal-library/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Nav from "./components/nav/Nav";
+import Jumbotron from "./components/Jumbotron/Jumbotron";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Jumbotron/>
     </div>
   );
 }
