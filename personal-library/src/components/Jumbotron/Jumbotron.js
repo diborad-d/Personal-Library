d3ced2ad42e2fccee36a2545ebe26cc11763b124
Dibora D <diborad@yahoo.com>
2020-08-07 21:33:22 -0500

    added style prop

--- personal-library/src/components/Jumbotron/Jumbotron.js
+++ personal-library/src/components/Jumbotron/Jumbotron.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Jumbotron = prop=> {
+const Jumbotron = prop => {
   return (
     <div style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }} className="jumbotron">
       {prop.children}
