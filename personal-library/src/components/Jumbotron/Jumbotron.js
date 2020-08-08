43b34ed49ce491ef8b484fb6f213f0f800d917b1
Dibora D <diborad@yahoo.com>
2020-08-07 21:15:13 -0500

    fixed children key effor

--- personal-library/src/components/Jumbotron/Jumbotron.js
+++ personal-library/src/components/Jumbotron/Jumbotron.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Jumbotron = children => {
+const Jumbotron = prop=> {
   return (
     <div style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }} className="jumbotron">
-      {children}
+      {prop.children}
     </div>
   );
 };
