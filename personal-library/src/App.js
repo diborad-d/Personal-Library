8f97f6ef3c667214f323b4a5715402d2043a8e1e
Dibora D <diborad@yahoo.com>
2020-08-07 20:50:56 -0500

    imported all the comps to Book.js and added Books comp. to App.js

--- personal-library/src/App.js
+++ personal-library/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Nav from "./components/nav/Nav";
+import Books from "./pages/Books";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Books />
     </div>
   );
 }
